216b97ada41b96b6696f76e908c119f1936bbb99
Ryllex <epostigo1999@gmail.com>
2023-10-30 12:16:39 -0500

    feature: plant service added

---
+++ src/services/plant-service.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const http = axios.create({
+    baseURL:"https://my-json-server.typicode.com/DevIOT-AgriPure/agripure-json/plants/",
+    headers: { "Content-type": "application/json" },
+})
+export class PlantServices{
+    getPlantInfoByCropId(id){
+        return http.get(""+id);
+    }
+    getAllPlants(){
+        return http.get("");
+    }
+    getResultsByPlantName(plantName){
+        return http.get("?name_like="+plantName);
+    }
+}
