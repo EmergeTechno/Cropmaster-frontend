d0b7f62034d02c982ab684b9f02fe98bf94c8c62
bruno <u201914974@upc.edu.pe>
2023-10-30 17:21:13 -0500

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './assets/main.css'
 import router from './router/index.js'
 import PrimeVue from 'primevue/config';
 
@@ -11,11 +10,52 @@ import "primevue/resources/primevue.min.css"                //core css
 import "primeicons/primeicons.css"
 
 import Button from 'primevue/button';
+import Avatar from 'primevue/avatar';
+import AutoComplete from 'primevue/autocomplete';
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
+import Dropdown from 'primevue/dropdown';
+import Dialog from 'primevue/dialog';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+import Tag from 'primevue/tag';
+import Slider from 'primevue/slider';
+import ProgressBar from 'primevue/progressbar';
+import Checkbox from 'primevue/checkbox';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import InputSwitch from 'primevue/inputswitch';
+
+
+
+
+
+
 
 const app = createApp(App)
 
 app.use(PrimeVue);
 app.use(router)
 app.component("pv-button",Button)
+app.component("pv-avatar",Avatar)
+app.component("pv-autoComplete",AutoComplete)
+app.component("pv-card",Card)
+app.component("pv-input",InputText)
+app.component("pv-dropdown",Dropdown)
+app.component("pv-dialog",Dialog)
+app.component("pv-dataTable",DataTable)
+app.component("pv-column",Column)
+app.component("pv-columnGroup",ColumnGroup)
+app.component("pv-row",Row)
+app.component("pv-tag",Tag)
+app.component("pv-slider",Slider)
+app.component("pv-progressBar",ProgressBar)
+app.component("pv-checkbox",Checkbox)
+app.component("pv-accordion",Accordion)
+app.component("pv-accordionTab",AccordionTab)
+app.component("pv-inputSwitch",InputSwitch)
+
 
 app.mount('#app')
